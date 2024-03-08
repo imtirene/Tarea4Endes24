d13399b3ac4eaf8918fb04d13d66ff8084293585
imtirene <imartost96@gmail.com>
2024-03-09 00:43:03 +0100

    try/catch

--- src/entidadFinanciera/Main.java
+++ src/entidadFinanciera/Main.java
@@ -11,6 +11,7 @@ import entidadFinanciera.CCuenta;
 public class Main {
 
     public static void main(String[] args) {
+        try{
         CCuenta objectoCuenta;
         double saldoActual;
         
@@ -18,7 +19,11 @@ public class Main {
         objectoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = objectoCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-        
+        } catch (Exception e) {
+            // Manejo de la excepción
+            System.out.println("Se produjo un error: " + e.getMessage());
+        }
+    
     
     }
 }
